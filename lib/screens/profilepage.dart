345a4e23a27c0bd203a057169fd97f49e17b29e4
vidhyadhar <vidhyadharreddyk@gmail.com>
2023-09-27 20:04:34 +0530

    On branch profile1 Your branch is up to date with 'origin/profile1'.
    Changes to be committed:
    	modified:   lib/screens/arpage.dart
    	modified:   lib/screens/homepage.dart
    	modified:   lib/screens/profilepage.dart

--- lib/screens/profilepage.dart
+++ lib/screens/profilepage.dart
@@ -1,40 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:rescuetrack/screens/arpage.dart';
-import 'package:rescuetrack/screens/irpage.dart';
+
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Center(
-        child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const arpage()),
-                );
-              },
-              style: ElevatedButton.styleFrom(backgroundColor: Colors.orange),
-              child: const Text("Agency Registration"),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const irpage()),
-              );
-              },
-              style: ElevatedButton.styleFrom(backgroundColor: Colors.orange),
-              child: const Text("Individual Registration"),
-            ),
-          ],
-        ),
-      ),
-    );
+    return const Placeholder();
   }
 }
